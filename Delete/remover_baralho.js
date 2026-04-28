2f9f711475a6578a1a83b0663de3fc9ae60080fc
JigsawCesar <felipeportela_@hotmail.com>
2026-04-28 12:00:24 -0300

    feat: criação do menu, read e delete

---
+++ Delete/remover_baralho.js
@@ -0,0 +1,9 @@
+const remover_baralho = (baralhos, flashcards, id) => {
+    const indice = baralhos.findIndex(baralho => baralho.id === id)
+
+    if (indice !== -1) {
+        baralhos.splice(indice, 1)
+    };
+};
+
+export default remover_baralho;
