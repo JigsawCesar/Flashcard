f015a8cbd7749dbac62725f76d8c669b3e2ba25d
JigsawCesar <felipeportela_@hotmail.com>
2026-04-28 12:09:54 -0300

    feat: criação do menu, read e delete

--- menu.js
+++ menu.js
@@ -6,14 +6,16 @@ ${roxo}=======================================
 ===== ${negrito}${amarelo}SISTEMA DE FLASHCARDS v1.0${reset} ${roxo}======
 =======================================
 ${roxo}⦙                                      ⦙
-${roxo}⦙ ${negrito}${amarelo}[1]${reset} - ${amarelo}Adicionar Baralho${reset}              ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[2]${reset} - ${amarelo}Adicionar Flashcard${reset}            ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[3]${reset} - ${amarelo}Listar Baralhos${reset}                ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[4]${reset} - ${amarelo}Listar Flashcards${reset}              ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[5]${reset} - ${amarelo}Atualizar Baralho${reset}              ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[6]${reset} - ${amarelo}Atualizar Flashcard${reset}            ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[7]${reset} - ${amarelo}Remover Baralho${reset}                ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[8]${reset} - ${amarelo}Remover Flashcard${reset}              ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[1]${reset}  - ${amarelo}Adicionar Baralho${reset}             ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[2]${reset}  - ${amarelo}Adicionar Flashcard${reset}           ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[3]${reset}  - ${amarelo}Listar Baralhos${reset}               ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[4]${reset}  - ${amarelo}Listar Flashcards${reset}             ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[5]${reset}  - ${amarelo}Atualizar Baralho${reset}             ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[6]${reset}  - ${amarelo}Atualizar Flashcard${reset}           ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[7]${reset}  - ${amarelo}Remover Baralho${reset}               ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[8]${reset}  - ${amarelo}Remover Flashcard${reset}             ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[9]${reset}  - ${amarelo}Buscar por Pergunta${reset}           ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[10]${reset} - ${amarelo}Buscar por Baralho${reset}            ${roxo}⦙
 ${roxo}⦙                                      ⦙
 ${roxo}⦙ ${vermelho}[0]${reset} - ${vermelho}Sair${reset}                           ${roxo}⦙
 ${roxo}=======================================${reset}`);
