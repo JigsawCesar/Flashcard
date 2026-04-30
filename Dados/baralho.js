b6b4bd3c6450cd2d949b17ee1f8a28f9bad8a9e1
wendel0001 <wendelsauterjj@gmail.com>
2026-04-30 10:58:11 -0300

    "...2"

--- Dados/baralho.js
+++ Dados/baralho.js
@@ -1,5 +1,13 @@
 const baralhos = [
-    { id: 1, titulo: "JavaScript" },
-    { id: 2, titulo: "Matemática" }
-    ];
-    export default baralhos;
+    
+    { id: 1, 
+      titulo: "JavaScript" 
+    },
+
+    { id: 2, 
+      titulo: "Matemática" 
+    }
+
+];
+
+export default baralhos;
