c45a9f0730ffadcf71f7c3a00b9c4900b21151b9
Apollo2210 <stormsiders021@gmail.com>
2026-04-30 11:04:41 -0300

    update error

--- Update/atualizar_baralho.js
+++ Update/atualizar_baralho.js
@@ -1,30 +1,29 @@
-const atualizar_baralho=(id,titulo)=>{
-    const indice = baralhos.findIndex(baralhos => baralhos.id === id)
+const atualizar_baralho = (id, novos_dados)=> {
+  const indice = baralhos.findIndex((baralhos) => baralhos.id === id);
+
+  if (indice === -1) {
+    console.log(" Erro: Usuario não encontrado... ");
+    return;
+  };
+};
+    if (novos_dados.titulo) {
+        let dados_em_uso = false;
+            for (let i = 0; i < baralhos.length; i++) {
+                let baralho_atual = baralhos[i];
     
-
-      if(indice ===-1){
-        console.log(" Erro: Usuario não encontrado... ");
-        return
+    
+    
+if(baralho_atual.titulo === novos_dados.titulo && baralho_atual.id !== id ) {
+     dados_em_uso = true;
+        break;
     }
-}
-if (novos_dados.titulo) {
-    let dados_em_uso = false
-    for(let i = 0; i <baralhos.length; i++){
-        let baralho_atual = baralhos[i]
-            if (baralho_atual.titulo === novos_dados.titulo && baralho_atual.id !== id) {
-dados_em_uso = true
-break
-    }
-
-}
-if(dados_em_uso) {
-
+  }
+  
+if (dados_em_uso) {
     console.log(" Erro: Os novos dados já estão em uso por outro usuario ");
-        return
-    
-    }
-    
-    }
-    baralhos[indice].id = novos_dados.id || baralhos[indice].id
-    baralhos[indice].titulo = novos_dados.titulo || baralhos[indice].titulo
-     
+    return;
+  }
+}
+baralhos[indice].id = novos_dados.id || baralhos[indice].id;
+baralhos[indice].titulo = novos_dados.titulo || baralhos[indice].titulo;
+console.log(" Baralho atualizado com sucesso ");
