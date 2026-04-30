33ea0f3352d4b531e68f7eec5c63bb3ce18b077e
JigsawCesar <felipeportela_@hotmail.com>
2026-04-30 12:08:45 -0300

    "feat: fazendo importações "

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 
 import { vermelho, roxo, ciano, reset } from "./cores_terminal.js"
+import baralhos from "./Dados/baralho.js";
+import flashcards from "./Dados/flashcard.js"
+import adicionar_baralho from "./Create/adicionar_baralho.js"
+import adicionar_flashcard from "./Create/adicionar_flashcard.js"
+import listar_baralho from "./Read/listar_baralho.js"
+import listar_flashcard from "./Read/listar_flashcard.js"
+import listar_por_baralho from "./Read/listar_por_baralho.js"
