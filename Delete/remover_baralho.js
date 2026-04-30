d4a755cc42d7a7672682418b44e5a2840d7d0a44
JigsawCesar <felipeportela_@hotmail.com>
2026-04-29 22:05:02 -0300

    fix: corrigir formatação e adicionar ponto e vírgula no remover_baralho

--- Delete/remover_baralho.js
+++ Delete/remover_baralho.js
@@ -1,8 +1,11 @@
 const remover_baralho = (baralhos, flashcards, id) => {
-    const indice = baralhos.findIndex(baralho => baralho.id === id)
+    
+    const indice = baralhos.findIndex(baralho => baralho.id === id);
 
     if (indice !== -1) {
-        baralhos.splice(indice, 1)
+        baralhos.splice(indice, 1);
+
+        return flashcards.filter(flashcard => flashcard.baralho_id !== id);
     };
 };
 
