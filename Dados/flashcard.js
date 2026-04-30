a466ebf5610e7b8966f27e726d02f602eeceb41e
wendel0001 <wendelsauterjj@gmail.com>
2026-04-30 09:58:20 -0300

    "..."

--- Dados/flashcard.js
+++ Dados/flashcard.js
@@ -3,4 +3,4 @@ const flashcards = [
     memória para armazenar valores.`, idBaralho: 1 },
     { id: 2, pergunta: "Quanto é 1 + 1?", resposta: "2", idBaralho: 2 }
     ];
-    export default flashcards;
+export default flashcards;
