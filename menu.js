d23f6bc20901356eeb6f46f1927f5d61bf006327
JigsawCesar <felipeportela_@hotmail.com>
2026-05-05 09:53:49 -0300

    "feat: adicionado a função de listar_por_baralho no menu.js"

--- menu.js
+++ menu.js
@@ -10,11 +10,12 @@ ${roxo}⦙ ${negrito}${amarelo}[1]${reset}  - ${amarelo}Adicionar Baralho${reset
 ${roxo}⦙ ${negrito}${amarelo}[2]${reset}  - ${amarelo}Adicionar Flashcard${reset}           ${roxo}⦙
 ${roxo}⦙ ${negrito}${amarelo}[3]${reset}  - ${amarelo}Listar Baralhos${reset}               ${roxo}⦙
 ${roxo}⦙ ${negrito}${amarelo}[4]${reset}  - ${amarelo}Listar Flashcards${reset}             ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[5]${reset}  - ${amarelo}Atualizar Baralho${reset}             ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[6]${reset}  - ${amarelo}Atualizar Flashcard${reset}           ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[7]${reset}  - ${amarelo}Remover Baralho${reset}               ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[8]${reset}  - ${amarelo}Remover Flashcard${reset}             ${roxo}⦙
-${roxo}⦙ ${negrito}${amarelo}[9]${reset}  - ${amarelo}Buscar por Pergunta${reset}           ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[5]${reset}  - ${amarelo}Listar por Baralho${reset}             ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[6]${reset}  - ${amarelo}Atualizar Baralho${reset}             ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[7]${reset}  - ${amarelo}Atualizar Flashcard${reset}           ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[8]${reset}  - ${amarelo}Remover Baralho${reset}               ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[9]${reset}  - ${amarelo}Remover Flashcard${reset}             ${roxo}⦙
+${roxo}⦙ ${negrito}${amarelo}[10]${reset}  - ${amarelo}Buscar por Pergunta${reset}           ${roxo}⦙
 ${roxo}⦙                                      ⦙
 ${roxo}⦙ ${vermelho}[0]${reset} - ${vermelho}Sair${reset}                           ${roxo}⦙
 ${roxo}=======================================${reset}`);
