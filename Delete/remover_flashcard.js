0c2af7c2e8ed2a53175c2126d975ecf7b6a2f94f
JigsawCesar <felipeportela_@hotmail.com>
2026-05-11 21:17:50 -0300

    feat: corrigir lógica de remoção de flashcard para tratamento de erro

--- Delete/remover_flashcard.js
+++ Delete/remover_flashcard.js
@@ -1,9 +1,14 @@
+import { negrito, vermelho, reset, rosa } from "../cores_terminal.js";
+
 const remover_flashcard = (flashcards, id) => {
     const indice = flashcards.findIndex(flashcard => flashcard.id === id)
 
-    if (indice !== -1) {
-        flashcards.splice(indice, 1)
-    };
+    if (indice === -1) {
+        console.log(`${negrito}${vermelho}⦙ Erro: Flashcard não encontrado!${reset}`);
+        return;
+    }
+
+    flashcards.splice(indice, 1);
 };
 
 export default remover_flashcard;
