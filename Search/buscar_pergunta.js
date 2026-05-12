6ef3072e743e1b16cda4665c9c226981e3334616
JigsawCesar <felipeportela_@hotmail.com>
2026-05-11 21:18:08 -0300

    feat: aprimorar função de busca de perguntas com mensagens mais claras e organização do código

--- Search/buscar_pergunta.js
+++ Search/buscar_pergunta.js
@@ -1,16 +1,18 @@
 import promptSync from "prompt-sync";
 import { vermelho, reset, ciano, negrito, rosa } from "../cores_terminal.js" 
+import listar_flashcard from "../Read/listar_flashcard.js";
 const prompt= promptSync();
 
 const buscar_pergunta = (flashcards) => {
 
-  let entrada = prompt(`${negrito}${ciano}⦙ Digite sua pesquisa: ${reset}`);
-  let filtro = flashcards.filter(flashcards => flashcards.pergunta.toLowerCase().includes(entrada.toLowerCase()))
+  let entrada = prompt(`${negrito}${ciano}⦙ Digite a pergunta que deseja pesquisar: ${reset}`);
+  let filtro = flashcards.filter(f => f.pergunta.toLowerCase().includes(entrada.toLowerCase()));
+  console.clear();
 
   if (filtro.length > 0){
 
-    console.log(`\n${negrito}${rosa}⦙ Alguns resultados encontrados: ${reset}`);
-    console.log(filtro);
+    console.log(`\n${negrito}${rosa}⦙ Resultados encontrados: ${reset}`);
+    listar_flashcard(filtro);
   
   } else {
 
