eb055c458e711b7f22acbde47b37bbf11a96c8e5
JigsawCesar <felipeportela_@hotmail.com>
2026-05-11 21:17:59 -0300

    feat: corrigir conversão de ID ao filtrar flashcards por baralho

--- Read/listar_por_baralho.js
+++ Read/listar_por_baralho.js
@@ -1,10 +1,11 @@
 import flashcards from "../Dados/flashcard.js";
 import { negrito, reset, vermelho } from "../cores_terminal.js";
+import listar_flashcard from "./listar_flashcard.js";
 
 const listar_por_baralho = (id_escolhido) => {
     
     // Filtra apenas os flashcards que possuem o ID do baralho selecionado
-    const filtrados = flashcards.filter(filtrado => filtrado.idBaralho === id_escolhido);
+    const filtrados = flashcards.filter(filtrado => filtrado.idBaralho === Number(id_escolhido));
 
     if (filtrados.length === 0) {
         console.log(`${negrito}${vermelho}\n⦙ Nenhum flashcard encontrado para este baralho!${reset}`);
