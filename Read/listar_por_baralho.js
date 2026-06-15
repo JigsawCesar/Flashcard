b097bf326d8b33aa58b82ce0b0bf1ff680c7dd6d
JigsawCesar <felipeportela_@hotmail.com>
2026-06-15 18:53:09 -0300

    feat: melhorar a verificação de existência do baralho e atualizar mensagens de console

--- Read/listar_por_baralho.js
+++ Read/listar_por_baralho.js
@@ -1,13 +1,21 @@
+import baralhos from "../Dados/baralho.js";
 import flashcards from "../Dados/flashcard.js";
-import { negrito, reset, vermelho } from "../cores_terminal.js";
+import { negrito, reset, vermelho, amarelo } from "../cores_terminal.js";
 import listar_flashcard from "./listar_flashcard.js";
 
 const listar_por_baralho = (id_escolhido) => {
     
+    const baralho_existe = baralhos.some(baralho => baralho.id === Number(id_escolhido));
+
+    if (!baralho_existe) {
+        console.log(`${negrito}${vermelho}\n⦙ Baralho não encontrado!${reset}`);
+        return;
+    }
+
     const filtrados = flashcards.filter(filtrado => filtrado.idBaralho === Number(id_escolhido));
 
     if (filtrados.length === 0) {
-        console.log(`${negrito}${vermelho}\n⦙ Nenhum flashcard encontrado para este baralho!${reset}`);
+        console.log(`${negrito}${amarelo}\n⦙ Este baralho não possui flashcards ainda.${reset}`);
     } else {
         listar_flashcard(filtrados);
     }
